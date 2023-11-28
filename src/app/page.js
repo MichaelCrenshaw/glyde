b1f01d0f76edbae2c5b4ddac35cb1eeeb7190a17
SirenKing <7884941+SirenKing@users.noreply.github.com>
2023-11-28 10:46:18 -0700

    Initial Navigation UI Commit
    The two navigation columns contain 'sliders' that should be animated to slide up-and-down to highlight the current selected element. Otherwise, it is also fine (for now) to hard-code the position of those sliders to match the current page.

--- src/app/page.js
+++ src/app/page.js
@@ -5,24 +5,45 @@ export default function Home() {
         <div class="body">
             <div class="left-bar">
                 <div class="header">
-                    <div class="header-image-logo"></div>
-                    <div class="header-logo-container"></div>
-                    <div class="lable-app-name"></div>
-                    <div class="header-name-container"></div>
+                    <div class="header-logo-container">
+                        <div class="app-logo"></div>
+                    </div>
+                    <div class="header-name-container">
+                        <div class="lable-app-name">GLYDE</div>
+                    </div>
                 </div>
                 <div class="navigation">
                     <div class="nav-1-column">
                         <div class="nav-buttons-container">
-                            <div class="nav-button"></div>
-                            <div class="nav-button"></div>
-                            <div class="nav-button"></div>
-                            <div class="nav-button"></div>
+                            <div class="nav-button">
+                                <div class="nav-button-image home-button"></div>
+                            </div>
+                            <div class="nav-button">
+                                <div class="nav-button-image profile-button"></div>
+                            </div>
+                            <div class="nav-button">
+                                <div class="nav-button-image friends-button"></div>
+                            </div>
+                            <div class="nav-button">
+                                <div class="nav-button-image settings-button"></div>
+                            </div>
+                        </div>
+                        <div class="nav-slider-1">
+                            <div class="nav-1-rounded-top"></div>
+                            <div class="nav-1-rounded-bottom"></div>
                         </div>
                     </div>
                     <div class="nav-2-column">
-                        <div class="search-bar"></div>
+                        <div class="search-bar">
+                            <input type="text" class="input-search-bar" placeholder="Search.."></input>
+                        </div>
                         <div class="chat-list">
-                            <div class="chat-list-item"></div>
+                            <div class="chat-list-item"># McCarthy</div>
+                            <div class="chat-list-item"># Work</div>
+                            <div class="chat-list-item selected"># James, Me</div>
+                            <div class="chat-list-item"># English 101</div>
+                            <div class="chat-list-item"># Home</div>
+                            <div class="chat-list-item"># Book Club</div>
                         </div>
                         <div class="nav-slider-2">
                             <div class="light-blue-selection-slider">
